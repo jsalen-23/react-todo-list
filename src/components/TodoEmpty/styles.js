0450903b2d578f7ea8969ea7e658c269d9ead7c9
Joseph Salen <jsalen.91@gmail.com>
2021-10-16 21:43:36 -0400

    fix emptylist size

--- src/components/TodoEmpty/styles.js
+++ src/components/TodoEmpty/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.section`
   width: 100%;
-  height: 60vh;
+  height: 70vh;
   margin: 0 auto;
   display: grid;
   place-items: center;
